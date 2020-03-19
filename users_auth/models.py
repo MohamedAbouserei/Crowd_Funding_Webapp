1257d1775bba91a4ffc0b2bf1cde5d4adfa3f5de
sabreensalama <sabreensalama29@gmail.com>
2020-03-19 11:22:14 +0200

    "update models picture and add phone"

--- users_auth/models.py
+++ users_auth/models.py
@@ -6,9 +6,11 @@ class Users(models.Model):
     last_name = models.CharField(null=False , max_length=50)
     email = models.EmailField(null=False,max_length=254)
     password = models.CharField(null=False, max_length=50)
+    us_phone=models.CharField(null=True,max_length=12)
     date_birth =models.DateField(null=True)
     faceboo_link= models.URLField(null=True)
-    picture_url = models.URLField(null=True)
+    picture = models.FileField(null=True,upload_to='pic_images/')
     created_date = models.DateField(auto_now_add=True)
     last_edit_date = models.DateField(auto_now=True)
+
 

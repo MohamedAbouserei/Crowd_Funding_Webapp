652de45b4bc169b2e8fca9cd50105870a24f26d1
MohamedAbouserei <medoaboserii@gmail.com>
2020-03-20 16:25:54 +0200

    lte admin added with temporary categories view added

--- users_auth/views.py
+++ users_auth/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    
+    return render(request, 'users_auth/index.html')
+
+def categories(request):
+    
+    return render(request, 'users_auth/categories.html')

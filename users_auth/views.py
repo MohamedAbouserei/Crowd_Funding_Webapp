f768ccf87e87904291a1a5c62a597eda6c8949f6
sabreensalama <sabreensalama29@gmail.com>
2020-03-21 16:07:30 +0200

    sign up functionality

--- users_auth/views.py
+++ users_auth/views.py
@@ -2,6 +2,28 @@ from django.shortcuts import render
 
 from Project.models import *
 from django.http import HttpResponse
+
+def home(request):
+    return  render(request,'users_auth/home.html')
+
+
+
+def sign_up(request):
+    if request.method == "POST":
+        form = New_users(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'users_auth/success.html', {"user": form })
+
+    else:
+        form = New_users()
+    return render(request, 'users_auth/sign_up.html', {'form': form})
+
+
+def thanks(request):
+    user=Users.objects.all()
+    return render(request,'users_auth/success.html',{"user": user })
+
 # Create your views here.
 def index(request):
     
